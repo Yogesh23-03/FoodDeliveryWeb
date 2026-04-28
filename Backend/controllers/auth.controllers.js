c359c750d7baf516b7c93e5da9f46703f9cf7877
Yogesh S <116181840+Yogesh23-03@users.noreply.github.com>
2026-04-28 14:59:46 +0530

    Update auth.controllers.js

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -36,8 +36,8 @@ const signUp = async (req, res) => {
         const token = await genToken(user._id)
 
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -67,8 +67,8 @@ const signIn = async (req, res) => {
         const token = await genToken(user._id)
 
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -157,8 +157,8 @@ const googleAuth=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
